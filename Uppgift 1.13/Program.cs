1ebe8414ab9b47caf90604dede522fe6fb19ec2b
elpe2005 <epedersen2005@gmail.com>
2022-09-14 12:10:16 +0200

    Klar med uppgift 1.13

--- Uppgift 1.13/Program.cs
+++ Uppgift 1.13/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Var snäll och skriv lönen av dina anställda. ");
+            int lön = int.Parse(Console.ReadLine());
+            int lön2 = lön + int.Parse(Console.ReadLine());
+            int lön3 = lön2 + int.Parse(Console.ReadLine());
+            Console.WriteLine(lön3);
+
+        }
+    }
+}
